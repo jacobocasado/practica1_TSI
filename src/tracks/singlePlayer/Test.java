74d8b5b6ee75fea7e0cee8b455f5613e06cb6a11
jcasado <casacojacobo@gmail.com>
2021-03-15 18:57:37 +0100

    Inicios de la práctica 1

--- src/tracks/singlePlayer/Test.java
+++ src/tracks/singlePlayer/Test.java
@@ -10,6 +10,11 @@ import tracks.ArcadeMachine;
  * Created with IntelliJ IDEA. User: Diego Date: 04/10/13 Time: 16:29 This is a
  * Java port from Tom Schaul's VGDL - https://github.com/schaul/py-vgdl
  */
+
+/**
+Jacobo Casado de Gracia. Práctica 1 de Técnicas de Sistemas Inteligentes
+*/
+
 public class Test {
 
     public static void main(String[] args) {
